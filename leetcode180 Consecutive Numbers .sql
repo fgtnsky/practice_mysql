80b894430b974bbd0a1b2e9b15b33e4869ea5fcb
WENJUN ZENG <zengx244@umn.edu>
2015-09-02 00:31:22 -0500

    Update leetcode180 Consecutive Numbers .sql

--- leetcode180 Consecutive Numbers .sql
+++ leetcode180 Consecutive Numbers .sql
@@ -16,16 +16,19 @@ For example, given the above Logs table, 1 is the only number that appears conse
 ***************************************************************************/
 
 
+#  Consecutive Numbers:
+create table Logs(Id int(11) NOT NULL AUTO_INCREMENT, Num int, PRIMARY KEY(Id));
+INSERT INTO Logs(Num) VALUES(1),(1),(1),(2),(1),(2),(2);
+
 # Method 1
-select Score, rank from
-(select  (Case WHEN @pre_score = Score THEN @rank := @rank WHEN @pre_score <> Score THEN @rank := @rank + 1 END) as rank
-		, @pre_score := Score as Score
-from (select Score from Scores order by Score desc) as sorted_score
-		, (select @rank:=0,@pre_score := -1000) as init) as DATA1
+select distinct L1.Num
+from Logs L1, Logs L2, Logs L3
+where L1.Id = L2.Id -1 and L2.Id = L3.Id -1 and L1.Num = L2.Num and L2.Num = L3.Num
 
 # Method 2
-select Scores.Score, count(*) as Rank
-from Scores , (select distinct Score from Scores order by Score desc) as Ranking
-where Scores.Score <= Ranking.Score
-group by Scores.Id
-order by Scores.Score desc;
+select distinct Num from
+(select (CASE WHEN @last_num = Num THEN @freq := @freq + 1
+						WHEN @last_num <> Num Then @freq := 1 END) as freqency
+						, @last_num := Num as Num
+from Logs join (select @last_num := -1000, @freq := 0) as init) as Freq
+where freqency>=3;
